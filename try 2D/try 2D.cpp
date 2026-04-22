6356fa7c2731ccab5c3c8418e99d67fd52c4ba62
Idluk <baxhuh@yandex.ru>
2026-04-22 23:05:02 +0500

    проверка возможностей хихи

--- try 2D/try 2D.cpp
+++ try 2D/try 2D.cpp
@@ -7,7 +7,7 @@ using namespace std;
 const double M_PI = 3.14;
 const int HEIGHT = 40;
 const int WIDTH = 200;
-const int FPS = 30;
+const int FPS = 60;
 
 int screen[HEIGHT][WIDTH] = { 0 };
 char text_buffer[HEIGHT][WIDTH] = { 0 };
@@ -16,7 +16,7 @@ bool n_layers[1000] = {0};
 
 struct Object {
     int x = 0, y = 0;
-    int width = 0, height = 0, d = 0;
+    int width = 0, height = 0, radius = 0;
     int angle_deg = 0, visibility = 100;
     int layer[HEIGHT][WIDTH] = { 0 };
     int slot = -1;
@@ -58,7 +58,8 @@ struct Object {
                     int screen_x = x + j;
                     int screen_y = y + i;
                     if (screen_x >= 0 && screen_y >= 0 && screen_x < WIDTH && screen_y < HEIGHT) {
-                        layer[screen_y][screen_x] = 1;
+                        if (rand() % 100 > (100 - visibility))
+                            layer[screen_y][screen_x] = 1;
                     }
                 }
             }
@@ -90,13 +91,12 @@ struct Object {
 
     void circle() {
         clear_layer();
-        int r = d / 2;
-        for (int i = y - r; i < y + r; i++) {
-            for (int j = x - r; j < x + r; j++) {
+        for (int i = y - radius; i < y + radius; i++) {
+            for (int j = x - radius; j < x + radius; j++) {
                 if (j < 0 or i < 0 or j >= WIDTH or i >= HEIGHT) {
                     continue;
                 }
-                if (pow(j - x, 2) + pow(i - y, 2) * 3 <= pow(r, 2)) {
+                if (pow(j - x, 2) + pow(i - y, 2)*3 <= pow(radius, 2)) {
                     if (rand() % 100 > (100 - visibility))
                         layer[i][j] = 1;
                 }
@@ -166,19 +166,14 @@ int main()
     int time_sec = 0;
     int time_rate = 0;
     bool f_sec, f_rate;
-    int rate = 2;
-    string text1 = "FPS:" + to_string(FPS);
-    string text2;
+    int rate = 10;
     int x, y;
     x = 10; y = 10;
     bool run = true;
     //переменные тела цикла
-    int temp_time_rate = 0;
-    int temp_time_sec = 0;
     Object obj;
-    Object obj2;
-    obj2.x = 40; obj2.y = 30; obj2.width = 10; obj2.height = 10;
-    obj.x = 15; obj.y = 15; obj.width = 15; obj.height = 10; obj.angle_deg = 0;
+    string text1, text2;
+    
     while (run) { //экран 200 x 40
         time_screen++;
         time_sec = time_screen / FPS; // изменение раз в секунду
@@ -190,16 +185,20 @@ int main()
         system("cls");
         //начало
         clear();
-        //draw_text(text1.c_str(), 0, 0); //fps
+        text1 = "Visibility:" + to_string(obj.visibility);
+        draw_text(text1.c_str(), 0, 0);
         for (int n = 0; n < 1000; n++) {
             if (n_layers[n])
                 text2 = "Layers:" + to_string(n);
         }
-        draw_text(text2.c_str(), 0, 0);
+        draw_text(text2.c_str(), 0, 1);
         //тело программы
-        obj.x += 1;
-        obj.quad();
-        obj2.quad();
+        if (f_rate) {
+            obj.x = 5 + time_rate*20 % 200 ; obj.y = 20; obj.width = 30; obj.height = 15;
+            obj.quad();
+        }
+        obj.visibility = time_rate*10 % 100;
+
         //конец
         draw();
         Sleep(1000 / FPS);
